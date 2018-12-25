56e351f5933758318ed8b1b73bc7210ba6331a08
JP Dillingham <jpdillingham@gmail.com>
2018-12-24 19:35:52 -0600

    dotnet new console, add .gitignore

---
+++ C#/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TicTacToe.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
