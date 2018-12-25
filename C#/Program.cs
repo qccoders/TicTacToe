b3c7957f6387d98fc7f27ef187f8e996bfa188d1
JP Dillingham <jpdillingham@gmail.com>
2018-12-24 21:15:04 -0600

    remove Cell struct, implement GetWinner()

--- C#/Program.cs
+++ C#/Program.cs
@@ -4,18 +4,6 @@ namespace TicTacToe.NET
 {
     class Program
     {
-        struct Cell
-        {
-            public Cell(int x, int y)
-            {
-                X = x;
-                Y = y;
-            }
-
-            public readonly int X;
-            public readonly int Y;
-        }
-
         static char[,] board;
 
         static void Main(string[] args)
@@ -31,47 +19,74 @@ namespace TicTacToe.NET
                 Console.WriteLine(Environment.NewLine + "Enter your choice in the format 'x,y' (zero based, left to right, top to bottom):");
 
                 var input = Console.ReadLine();
-                Cell inputCell;
+                int x, y;
 
                 try
                 {
                     var nums = input.Split(',');
 
-                    var x = int.Parse(nums[0]);
-                    var y = int.Parse(nums[1]);
+                    x = int.Parse(nums[0]);
+                    y = int.Parse(nums[1]);
 
                     if (x < 0 || x > 2 || y < 0 || y > 2)
                     {
                         throw new ArgumentException();
                     }
-
-                    inputCell = new Cell(x, y);
                 }
                 catch (Exception)
                 {
-                    Console.WriteLine("Invalid input!  Try again.");
+                    Console.WriteLine(Environment.NewLine + "Invalid input!  Try again.");
                     continue;
                 }
 
-                if (!IsCellAvailable(inputCell))
+                if (!IsCellAvailable(x, y))
                 {
-                    Console.WriteLine("That cell is already selected.");
+                    Console.WriteLine(Environment.NewLine + "That cell is already selected.");
                     continue;
                 }
 
-                board[inputCell.Y, inputCell.X] = 'X';
+                board[y, x] = 'X';
 
-            } while (!GameOver());
+            } while (GetWinner() == null);
+
+            Console.WriteLine($"{Environment.NewLine}{GetWinner()} is the winner! Here's the final board:{Environment.NewLine}");
+            PrintBoard();
         }
 
-        static bool IsCellAvailable(Cell cell)
+        static bool IsCellAvailable(int x, int y)
         {
-            return board[cell.Y, cell.X] == ' ';
+            return board[y, x] == ' ';
         }
 
-        static bool GameOver()
+        static char? GetWinner()
         {
-            return false;
+            int[,,] combos = new int[8, 3, 2] {
+                { { 0, 0 }, { 0, 1 }, { 0, 2 } },
+                { { 1, 0 }, { 1, 1 }, { 1, 2 } },
+                { { 2, 0 }, { 2, 1 }, { 2, 2 } },
+                { { 0, 0 }, { 1, 0 }, { 2, 0 } },
+                { { 0, 1 }, { 1, 1 }, { 2, 1 } },
+                { { 0, 2 }, { 1, 2 }, { 2, 2 } },
+                { { 0, 0 }, { 1, 1 }, { 2, 2 } },
+                { { 2, 0 }, { 1, 1 }, { 0, 2 } },
+            };
+
+            for (int i = 0; i < 8; i++)
+            {
+                var combo = new char[] { ' ', ' ', ' ' };
+
+                for (int j = 0; j < 3; j++)
+                {
+                    combo[j] = board[combos[i, j, 1], combos[i, j, 0]];
+                }
+
+                if (combo[0] != ' ' && combo[0] == combo[1] && combo[1] == combo[2])
+                {
+                    return combo[0];
+                }
+            }
+
+            return null;
         }
 
         static void PrintBoard()
