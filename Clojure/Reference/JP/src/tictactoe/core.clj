085ac34051d69c543f1c8adaa7e08934d724c1bc
JP Dillingham <jpdillingham@gmail.com>
2018-12-31 08:22:17 -0600

    add game loop

--- Clojure/Reference/JP/src/tictactoe/core.clj
+++ Clojure/Reference/JP/src/tictactoe/core.clj
@@ -22,32 +22,42 @@
     (if (< i 2) (println "\n-+-+-"))))
 
 (defn -main [& args]
-  (println "Welcome to QC Coders' Tic Tac Toe! You're 'X' and you'll go first.")
-  (initboard)
-
   (loop []
-    (println "\nHere's the current board:\n")
-    (printboard)
+    (println "Welcome to QC Coders' Tic Tac Toe! You're 'X' and you'll go first.")
+    (initboard)
 
-    (println "\n\nEnter your choice in the format 'x,y' (zero based, left to right, top to bottom):")
+    (loop []
+      (println "\nHere's the current board:\n")
+      (printboard)
+
+      (println "\n\nEnter your choice in the format 'x,y' (zero based, left to right, top to bottom):")
+      (def input (read-line))
+      (def coords (clojure.string/split input #","))
+
+      (def x (clojure.edn/read-string (get coords 0)))
+      (def y (clojure.edn/read-string (get coords 1)))
+
+      (when 
+        (or 
+          (< x 0) (> x 2) (< y 0) (> y 2)) 
+            (println "Invalid input!  Try again.")
+            (System/exit 0))
+
+      (when (not= (get-in board [y x]) \ )
+        (println "That cell is already selected.")
+        (System/exit 0))
+
+      (def board (assoc-in board [y x] \X))
+      (if (= (getWinner) nil)
+        (recur)
+        (do 
+          (println "Here's the final board:") 
+          (printboard))))
+
+    (println "\nPress Enter to play again or x + Enter to exit.")
     (def input (read-line))
-    (def coords (clojure.string/split input #","))
-
-    (def x (clojure.edn/read-string (get coords 0)))
-    (def y (clojure.edn/read-string (get coords 1)))
-
-    (when 
-      (or 
-        (< x 0) (> x 2) (< y 0) (> y 2)) 
-          (println "Invalid input!  Try again.")
-          (System/exit 0))
-
-    (when (not= (get-in board [y x]) \ )
-      (println "That cell is already selected.")
-      (System/exit 0))
-
-    (def board (assoc-in board [y x] \X))
-    (if (= (getWinner) nil)
+    (if (= "" input)
       (recur)
-      (println "Game over"))))
+      (System/exit 0)))
+)
 
