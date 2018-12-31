739f4c2653885b6936b33509d6a7f4c02358b5ee
JP Dillingham <jpdillingham@gmail.com>
2018-12-30 19:05:57 -0600

    parse two integers from input

--- Clojure/Reference/JP/src/tictactoe/core.clj
+++ Clojure/Reference/JP/src/tictactoe/core.clj
@@ -1,6 +1,12 @@
 (ns tictactoe.core
   (:gen-class))
 
+; supporting stuff not included in the reference implementation
+(defn exit []
+  (println "Invalid input!  Try again.")
+  (System/exit 0))
+
+; begin reference implementation
 (def board nil)
 
 (defn initboard []
@@ -23,6 +29,18 @@
   (initboard)
   (println "\nHere's the current board:\n")
   (printboard)
+
   (println "\n\nEnter your choice in the format 'x,y' (zero based, left to right, top to bottom):")
   (def input (read-line))
-  (println input))
+  (def coords (clojure.string/split input #","))
+
+  (def x (clojure.edn/read-string (get coords 0)))
+  (def y (clojure.edn/read-string (get coords 1)))
+
+  (when (< x 0) (exit))
+  (when (> x 2) (exit))
+  (when (< y 0) (exit))
+  (when (> y 2) (exit))
+
+  (println "Your input looks valid so far..."))
+
